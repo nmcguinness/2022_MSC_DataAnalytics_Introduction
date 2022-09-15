68fbc50aefc8ca34430e64975d5e66169852c257
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-09-15 19:34:48 +0100

    Initial commit

---
+++ Examples/1_GettingStarted.R
@@ -0,0 +1,26 @@
+# Data Anaytics....
+# Author: NMCG
+# Date: 15/9/22
+# Bugs: None
+# Keywords: data types, control flow, data structures
+
+#automatically clear console
+cat("\014")
+
+# interpreter automatically infers the data type at initialization
+year <- 2022
+year
+
+height <- 1.89
+height
+
+isRaining <- TRUE
+isRaining
+
+title <- "Data Analytics"
+title
+
+strOut <- paste("The current year is", year)
+print(strOut)
+
+
