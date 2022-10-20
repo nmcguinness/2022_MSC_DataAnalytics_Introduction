40f3c2773ac4afc3ae07648325d19760d13467a0
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-10-20 13:23:07 +0100

    Update 7_BasicPlots.R

--- Examples/7_BasicPlots.R
+++ Examples/7_BasicPlots.R
@@ -4,10 +4,12 @@
 #' @bugs None
 #' @keywords strip, histogram, boxplot, scatterplot
 
-# query/list available data
-# data()
+# query/list available data #################################################
 
-# examine data
+data()
+
+# examine data ##############################################################
+
 chickwts
 
 # access individual columns by name
@@ -26,24 +28,44 @@ tail(chickwts, n = 12)
 # show basic statistical summary (median, Q1,2,3 etc)
 summary(chickwts)
 
-# stripchart with title and plotting character set
-stripchart(chickwts$weight, main = "Strip Chart of Chick Weights",
+
+# stripchart #################################################################
+
+# title and plotting character set
+stripchart(chickwts$weight, 
+           main = "Strip Chart of Chick Weights",
            pch = 21)
 
-# histogram with title and xy labels and color
+# histogram #################################################################
+
+# title and xy labels and color
 hist(chickwts$weight, main = "Frequency of Chick Weights",
      xlab = "Chick Weight (grams)",
      ylab = "Weight Range Frequency",
      col = "orange")
 
-# boxplot with title and color
+# boxplot ###################################################################
+
 boxplot(chickwts$weight)
 
-# multiple boxplot with title and color
-boxplot(chickwts$weight~chickwts$feed, main = "Chick Weights in Grams",
+# box-plot (multiple categories using ~) ####################################
+
+boxplot(chickwts$weight~chickwts$feed, 
+        main = "Chick Weights in Grams",
         col = rgb(0.75, 0.5, 0.75))
 
-# remember to get help on a function just type ? before function and hit Enter in the Console (e.g. ?pch)
+# pie chart #################################################################
+
+sunshinePerDay <- c(6, 12, 17, 13)
+cities <- c("dublin", "paris", "narvik", "caracas")
+
+# uncomment the parameters (i.e. edges, main, border) to see what each parameter does
+pie(x = sunshinePerDay, 
+    labels = cities, 
+    edges = 32,
+    main = "Hours Sunshine on 21st June 2022", 
+    border = FALSE,
+    col = c("red","blue","green","orange"))
 
 
 
