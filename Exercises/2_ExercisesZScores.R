e7ea313adebfa1401fda8eca4b2ab2f54dd10c93
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-11-29 17:34:40 +0000

    :memo: Docs - added exercises

---
+++ Exercises/2_ExercisesZScores.R
@@ -0,0 +1,20 @@
+#' @title 2_ExercisesZScores
+#' @description Demonstrates calculation of Z-score using pnorm
+#' @author NMCG
+#' @bugs None
+#' @keywords z-score, normal distribution, pnorm
+
+# Construct/enter normally distributed data
+
+# Generate mean, median, mode, and standard deviation
+
+# Carry out normality tests
+
+# What is the probability of finding person with...?
+
+# What is the range for 68% of respondents centred around the median?
+
+# What value of X is necessary to be in the top 95% of the population?
+
+res <- pnorm(1, mean = 0, sd = 1, lower.tail = FALSE)
+res
