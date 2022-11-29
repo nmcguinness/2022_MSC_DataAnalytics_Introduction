5eb02391893cfa0e5834a4b95d1d9887d6b0224a
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-11-29 18:32:42 +0000

    :memo: Docs - adding exercise 2

--- Exercises/2_ExercisesZScores.R
+++ Exercises/2_ExercisesZScores.R
@@ -5,10 +5,22 @@
 #' @keywords z-score, normal distribution, pnorm
 
 # Construct/enter normally distributed data
+movie_ratings <- c(1,2,3,4,5,6,7,8,9,10)
 
 # Generate mean, median, mode, and standard deviation
+movie_mean <- mean(movie_ratings)
+movie_mean
+
+movie_sd <- sd(movie_ratings)
+movie_sd
 
 # Carry out normality tests
+hist(movie_ratings)
+
+qqnorm(movie_ratings, pch = 2, frame = FALSE, datax = TRUE)
+qqline(movie_ratings, col = "darkorchid", lwd = 1, datax = TRUE)
+
+
 
 # What is the probability of finding person with...?
 
